94a4794b196284d45dd19a216bd073f264e34c2f
Doralee <doralee@starlux-airlines.com>
2020-06-03 15:33:43 +0800

    增加Element-UI/Bootstrap/babel-plugin-component

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import store from '@/store'
 import VueI18n from 'vue-i18n'
 import i18n from '@/i18n'
 import mixin from '@/mixins/global'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(ElementUI);
 Vue.mixin(mixin)
 Vue.use(VueI18n)
 Vue.config.productionTip = false
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>{{ $t('aboutpage')}}</h1>
+    <el-button type="primary">測試Eelement-ui</el-button>
   </div>
 </template>
