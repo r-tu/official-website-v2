1332475cb95b6bec086c24170ae7728164e5abb7
Tommy Lin <tommylin@starlux-airlines.com>
2020-06-03 14:19:41 +0800

    fix no lang in url redirect

--- src/router/index.js
+++ src/router/index.js
@@ -20,10 +20,11 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  const langRegex = /^\/[a-z]{2}-[A-Z]{2}/;
   if (to.path === '/') { // 當網址從 http://localhost:8080/ 直接進入時
     next({path: '/' + (location.pathname.split('/')[1] || process.env.VUE_APP_I18N_LOCALE)}); // 套上語系路徑 http://localhost:8080/zh-TW/
-  } else if (!/^\/[a-z]{2}-[A-Z]{2}/.test(to.fullPath)) { // 沒有語系路徑開頭 
-    next({path: '/' + (location.pathname.split('/')[1] || from.params.lang) + to.path}); // 套上語系路徑 http://localhost:8080/zh-TW/ 並加上後續要去的位置
+  } else if (!langRegex.test(to.fullPath)) { // 沒有語系路徑開頭 
+    next({path: '/' + ( !langRegex.test(location.pathname) ? process.env.VUE_APP_I18N_LOCALE : location.pathname.split('/')[1] || from.params.lang) + to.path}); // 套上語系路徑 http://localhost:8080/zh-TW/ 並加上後續要去的位置
   } else {
     next();
   }
