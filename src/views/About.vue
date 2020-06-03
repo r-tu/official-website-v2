9111fd39528be0568ec779d55373af20f778ac6d
Hsuan <jh558125@gmail.com>
2020-06-03 15:51:42 +0800

    add axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,20 @@
     <h1>{{ $t('aboutpage')}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  created() {
+    //test axios
+    this.axios
+      .get("https://w5q6k.sse.codesandbox.io/api/v1/cumulative")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
