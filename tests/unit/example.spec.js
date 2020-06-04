7b5665ade0387d8f94df8d32763e441b96e01886
Hsuan <jh558125@gmail.com>
2020-06-04 09:17:14 +0800

    jest with i18n

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,8 +3,13 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'message';
+    
     const wrapper = shallowMount(HelloWorld, {
+      mocks: {
+        //$t: () => '自訂string'
+        $t: (key) => key //i18n key
+      },
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
