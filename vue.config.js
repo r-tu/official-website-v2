b34046c9206f6c67a7f355a27bebcc092489b55c
Tommy Lin <tommylin@starlux-airlines.com>
2020-06-10 23:04:50 +0800

    add deploy env file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const dotenv = require('dotenv')
+switch (process.env.DEPLOY) {
+  case 'uat':
+    dotenv.config({ path: '.env.uat' })
+    console.log('loaded .env.uat')
+    break
+  case 'staging':
+    dotenv.config({ path: '.env.staging' })
+    console.log('loaded .env.staging')
+    break
+  case 'master':
+    dotenv.config({ path: '.env.master' })
+    console.log('loaded .env.master')
+    break
+  default:
+    dotenv.config({ path: '.env.develop' })
+    console.log('loaded .env.develop')
+}
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
   devServer: {
